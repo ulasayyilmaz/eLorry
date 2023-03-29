e257f7361e4697fa5610f1080ce2047c8cd20bf8
Ulas Ayyilmaz <iuaa2020@mymail.pomona.edu>
2023-03-29 16:40:07 +0300

    put the database + use secret variable

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+from database import create_put_requestdb, put_request
+
+app = Flask(__name__)
+
+
+ 
+
+
+@app.route("/")
+def myapp():
+    #cursor = mysql.get_db().cursor()
+    #cursor.execute('''CREATE TABLE put_request(VARCHAR CompanyName,VARCHAR Material, VARCHAR DepartureLocation, DATETIME DepartureTime, VARCHAR ArrivalLocation, DATETIME ArrivalTime, VARCHAR Phone);''')
+    #cursor.execute('''INSERT INTO put_resuest (CompanyName,Material,DepartureLocation,DepartureTime,ArrivalLocation,ArrivalTime,Phone) VALUES ('CHEP','Palette', 'Aydin', '2023-04-12 08:15:00','Izmir', '2023-04-14 02:00:00', '552-256-8080') ''')
+    #rv = cursor.fetchall()
+    #create_put_requestdb()
+    requests = put_request()
+    return str(requests)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
---
+++ database.py
@@ -0,0 +1,27 @@
+from sqlalchemy import create_engine, text
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+engine = create_engine(os.getenv(ENGINEKEY), 
+connect_args={
+    "ssl": {
+        "ca": os.getenv(ENGINECONFIG),
+    }
+})
+
+
+def create_put_requestdb():
+    with engine.connect() as conn:
+        conn.execute(text("CREATE TABLE put_request(CompanyName VARCHAR);"))
+
+def put_request():
+    with engine.connect() as conn:
+        conn.execute(text("INSERT INTO requests (CompanyName) VALUES ('CHEP');"))
+        result = conn.execute(text("SELECT * FROM requests"))
+        row = result.all()
+        if len(row) == 0:
+            return None
+        else:
+            return row[0]._mapping
